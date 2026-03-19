c3788b768f5eccf148d3257ed34c8c0c1f413668
leofigueredo2008-beep <leofigueredo.2008@gmail.com>
2026-03-19 11:42:27 -0300

    Aula 02 variaveis

---
+++ aula02/aula02a.py
@@ -0,0 +1,28 @@
+print("ola mundo")
+
+print("7"+"4") #concatenando strings
+
+'''cometários de multiplas 
+linhas'''
+
+nome = "Bob"
+idade = "80"
+altura = "1.76"
+print(nome)
+print(idade)
+print(altura)
+
+print(f"olá,{nome}!!!")
+
+nome = input("Digite o seu nome: ")
+print(nome)
+idade=int(input("digite sua idade"))
+
+print(idade + 1)
+
+ano_nascimento = 2000
+ano_atual = 2026
+idade = ano_atual - ano_nascimento
+print(f"sua idade é: {idade}")
+
+
