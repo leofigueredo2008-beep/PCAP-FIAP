15331826bd799b129923061e60712298c974fe14
leofigueredo2008-beep <leofigueredo.2008@gmail.com>
2026-04-09 11:11:13 -0300

    Aula 2 revisão Aula 2 sobre funçoes Aula 2 sobre modulos

---
+++ Aula 03-modulos-condicionais/funcoes.py
@@ -0,0 +1,18 @@
+def print_lyrics():
+    print("I ain't gonna live forever")
+    print ("i just want to live while I'm alive")
+
+print_lyrics()
+
+def boas_vindas(nome):
+    print(f"olá, {nome}!! Seja bem-vindo")
+
+nome_digitado = input("digite seu nome: ")
+boas_vindas(nome_digitado)
+
+
+def soma(num_a,num_b):
+    soma = num_a + num_b
+    return soma
+
+print(soma(10,5))
---
+++ Aula 03-modulos-condicionais/modulos.py
@@ -0,0 +1,13 @@
+import math
+
+num = 18
+raiz = math.sqrt(num)
+print(f"A raiz de {num} é {raiz:.2f}")
+
+import random
+
+num_random=random.random()
+print(num_random)
+
+num_rand_int=random.randint(1,10)
+print(num_rand_int)
