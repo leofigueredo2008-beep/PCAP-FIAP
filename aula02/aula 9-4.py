90525b43759b8ef3fcb362a83a95b40f32d8c8c5
leofigueredo2008-beep <leofigueredo.2008@gmail.com>
2026-04-09 11:10:27 -0300

    Aula 2 revisão

---
+++ aula02/aula 9-4.py
@@ -0,0 +1,18 @@
+num1 = 5
+num2 = 2
+print(type(num1), type(num2))
+
+operacao =num1/num2
+print(operacao, type(operacao))
+
+idade = 20
+print(idade >= 21)
+
+maior_idade = idade>= 18
+print(maior_idade, type(maior_idade))
+
+
+nome1 = "marcos"
+nome2 = "Marcos"
+
+print(nome1.lower()==nome2.lower())
