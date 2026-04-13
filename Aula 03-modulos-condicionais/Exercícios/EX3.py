45462413f79df1f5b00043a84cf45e2d14b508aa
leofigueredo2008-beep <leofigueredo.2008@gmail.com>
2026-04-13 09:29:35 -0300

    Exercício 3 feito

---
+++ Aula 03-modulos-condicionais/Exercícios/EX3.py
@@ -0,0 +1,9 @@
+num_1=int(input("Digite um número: "))
+num_2=int(input("Digite outro número: "))
+
+if num_1>num_2:
+    print(f"O {num_1} é maior que o {num_2}")
+elif num_1<num_2:
+    print(f"O {num_2} é maior que o {num_1}")
+else:
+    print("Os dois números dados são iguais")
