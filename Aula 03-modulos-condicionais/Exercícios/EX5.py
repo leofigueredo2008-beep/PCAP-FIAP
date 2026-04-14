ae8b1d2240df1d4404164d40f3422d9671bc6323
leofigueredo2008-beep <leofigueredo.2008@gmail.com>
2026-04-14 10:00:33 -0300

    Exercício 5 finalizado

--- Aula 03-modulos-condicionais/Exercícios/EX5.py
+++ Aula 03-modulos-condicionais/Exercícios/EX5.py
@@ -9,5 +9,13 @@ elif  A%5 == 0 and B%5 == 0:
     print("Os valores são multiplos entre si")
 elif  A%7 == 0 and B%7 == 0:
     print("Os valores são multiplos entre si")
+elif  A%11 == 0 and B%11 == 0:
+    print("Os valores são multiplos entre si")
+elif  A%13 == 0 and B%13 == 0:
+    print("Os valores são multiplos entre si")
+elif  A%17 == 0 and B%17 == 0:
+    print("Os valores são multiplos entre si")
+elif  A%19 == 0 and B%19 == 0:
+    print("Os valores são multiplos entre si")
 else:
     print("Os valores não são multiplos")
