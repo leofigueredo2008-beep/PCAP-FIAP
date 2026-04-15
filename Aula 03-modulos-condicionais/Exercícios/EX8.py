1647a838cfdc0148bcf0bf10ba9de8ccde4d6259
leofigueredo2008-beep <leofigueredo.2008@gmail.com>
2026-04-15 10:37:31 -0300

    Exercício 8 finalizado

--- Aula 03-modulos-condicionais/Exercícios/EX8.py
+++ Aula 03-modulos-condicionais/Exercícios/EX8.py
@@ -7,3 +7,29 @@ if Sal<=280.0:
     print(f"Seu salário vai ser aumentado em 20%")
     print(f"{X:.2f} reais vai ser adicionado no seu salário")
     print(f"Seu novo salário é de {Sal_Novo:.2f} reais")
+
+elif Sal>280.0 and Sal<700.0:
+    X_2=Sal*0.15
+    Sal_Novo_2=Sal+X_2
+    print(f"Seu salário era de: {Sal:.2f}")
+    print(f"Seu salário vai ser aumentado em 15%")
+    print(f"{X_2:.2f} reais vai ser adicionado no seu salário")
+    print(f"Seu novo salário é de {Sal_Novo_2:.2f} reais")
+
+elif Sal>=700.0 and Sal<1500.00:
+    X_3=Sal*0.10
+    Sal_Novo_3=Sal+X_3
+    print(f"Seu salário era de: {Sal:.2f}")
+    print(f"Seu salário vai ser aumentado em 10%")
+    print(f"{X_3:.2f} reais vai ser adicionado no seu salário")
+    print(f"Seu novo salário é de {Sal_Novo_3:.2f} reais")
+
+else:
+    X_4=Sal*0.05
+    Sal_Novo_4=Sal+X_4
+    print(f"Seu salário era de: {Sal:.2f}")
+    print(f"Seu salário vai ser aumentado em 5%")
+    print(f"{X_4:.2f} reais vai ser adicionado no seu salário")
+    print(f"Seu novo salário é de {Sal_Novo_4:.2f} reais")
+
+
